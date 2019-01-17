15acd8a029eb26a2275d84a165e3cfc7bcb88a6d
Nicolás Restrepo Torres <nrestrepot@unal.edu.co>
2019-01-17 23:20:56 +0100

    Set Vue Router and install Axios

---
+++ src/components/Articles.vue
@@ -0,0 +1,30 @@
+<template>
+ <div id="articles">
+  <div v-for="(article) in articles" :key="article.id">
+   <p>{{article.name}} is my {{article.relationship}}</p>
+  </div>
+ </div>
+</template>
+
+<script>
+  import axios from 'axios'
+  export default {
+    data() {
+      return {
+        articles: [],
+        name: [],
+        relationship: [],
+        errors: []
+      }
+    },
+    created() {
+      axios.get('localhost:3000/articles')
+        .then(response => {
+        .this.articles = response.data
+        })
+        .catch(e => {
+          this.error.push(e)
+        })
+    },
+  }
+</script>
